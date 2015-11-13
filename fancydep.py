c7ce1179465db344826b8a2a63dce031c84b7634
EmilyK <emilyk@Emilys-MacBook-Pro.local>
2015-11-13 14:17:57 -0500

    adding notes for fancydep

--- fancydep.py
+++ fancydep.py
@@ -1,24 +1,22 @@
 """
 some ideas for part 3... 
 
-- add features, including trigram feature []
+- add features, including trigram feature [http://dl.acm.org/citation.cfm?id=2002777]
 
-- other paper showed dynamic explore oracle worked better
+- other paper showed dynamic explore oracle worked better [http://www.aclweb.org/anthology/C12-1059]
+
 - could try randomizing the order the training instances are chosen 
 - could try the larger training set
+
 - any combination of these
-
-
 """
 
 # call this before instead of using true next move at end of loop
-def chooseNextExplore(i, t): 
-    if (i > k and rand() > p): 
-        return predMove
-    else 
-        return chooseNextAmb(i, t)
-
-
+# def chooseNextExplore(i, t):
+#     if (i > k and rand() > p):
+#         return predMove
+#     else
+#         return chooseNextAmb(i, t)
         
     
         
